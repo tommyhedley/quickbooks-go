addc17c9114236ec2bc2d68cef65664e111dc714
tommyhedley <tommyhedley@gmail.com>
2025-03-07 21:09:33 -0800

    added batch and cdc extractor functions

--- batch.go
+++ batch.go
@@ -2,6 +2,7 @@ package quickbooks
 
 import (
 	"fmt"
+	"reflect"
 	"time"
 )
 
@@ -126,4 +127,204 @@ func (c *Client) BatchRequest(params RequestParameters, batchRequests []BatchIte
 	}
 
 	return allResponses, nil
+}
+
+func BatchEntityExtractor[T any](response *BatchItemResponse) (T, bool) {
+	var zero T
+	typeName := reflect.TypeOf(zero).Name()
+
+	switch typeName {
+	case "Account":
+		if !reflect.ValueOf(response.Account).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Account{}) {
+			return reflect.ValueOf(response.Account).Interface().(T), true
+		}
+	case "Bill":
+		if !reflect.ValueOf(response.Bill).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Bill{}) {
+			return reflect.ValueOf(response.Bill).Interface().(T), true
+		}
+	case "BillPayment":
+		if !reflect.ValueOf(response.BillPayment).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(BillPayment{}) {
+			return reflect.ValueOf(response.BillPayment).Interface().(T), true
+		}
+	case "Class":
+		if !reflect.ValueOf(response.Class).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Class{}) {
+			return reflect.ValueOf(response.Class).Interface().(T), true
+		}
+	case "CreditMemo":
+		if !reflect.ValueOf(response.CreditMemo).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(CreditMemo{}) {
+			return reflect.ValueOf(response.CreditMemo).Interface().(T), true
+		}
+	case "Customer":
+		if !reflect.ValueOf(response.Customer).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Customer{}) {
+			return reflect.ValueOf(response.Customer).Interface().(T), true
+		}
+	case "CustomerType":
+		if !reflect.ValueOf(response.CustomerType).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(CustomerType{}) {
+			return reflect.ValueOf(response.CustomerType).Interface().(T), true
+		}
+	case "Deposit":
+		if !reflect.ValueOf(response.Deposit).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Deposit{}) {
+			return reflect.ValueOf(response.Deposit).Interface().(T), true
+		}
+	case "Employee":
+		if !reflect.ValueOf(response.Employee).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Employee{}) {
+			return reflect.ValueOf(response.Employee).Interface().(T), true
+		}
+	case "Estimate":
+		if !reflect.ValueOf(response.Estimate).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Estimate{}) {
+			return reflect.ValueOf(response.Estimate).Interface().(T), true
+		}
+	case "Invoice":
+		if !reflect.ValueOf(response.Invoice).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Invoice{}) {
+			return reflect.ValueOf(response.Invoice).Interface().(T), true
+		}
+	case "Item":
+		if !reflect.ValueOf(response.Item).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Item{}) {
+			return reflect.ValueOf(response.Item).Interface().(T), true
+		}
+	case "Payment":
+		if !reflect.ValueOf(response.Payment).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Payment{}) {
+			return reflect.ValueOf(response.Payment).Interface().(T), true
+		}
+	case "PaymentMethod":
+		if !reflect.ValueOf(response.PaymentMethod).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(PaymentMethod{}) {
+			return reflect.ValueOf(response.PaymentMethod).Interface().(T), true
+		}
+	case "Purchase":
+		if !reflect.ValueOf(response.Purchase).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Purchase{}) {
+			return reflect.ValueOf(response.Purchase).Interface().(T), true
+		}
+	case "ReimburseCharge":
+		if !reflect.ValueOf(response.ReimburseCharge).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(ReimburseCharge{}) {
+			return reflect.ValueOf(response.ReimburseCharge).Interface().(T), true
+		}
+	case "TaxCode":
+		if !reflect.ValueOf(response.TaxCode).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(TaxCode{}) {
+			return reflect.ValueOf(response.TaxCode).Interface().(T), true
+		}
+	case "TaxRate":
+		if !reflect.ValueOf(response.TaxRate).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(TaxRate{}) {
+			return reflect.ValueOf(response.TaxRate).Interface().(T), true
+		}
+	case "Term":
+		if !reflect.ValueOf(response.Term).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Term{}) {
+			return reflect.ValueOf(response.Term).Interface().(T), true
+		}
+	case "TimeActivity":
+		if !reflect.ValueOf(response.TimeActivity).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(TimeActivity{}) {
+			return reflect.ValueOf(response.TimeActivity).Interface().(T), true
+		}
+	case "Vendor":
+		if !reflect.ValueOf(response.Vendor).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(Vendor{}) {
+			return reflect.ValueOf(response.Vendor).Interface().(T), true
+		}
+	case "VendorCredit":
+		if !reflect.ValueOf(response.VendorCredit).IsZero() && reflect.TypeOf(zero) == reflect.TypeOf(VendorCredit{}) {
+			return reflect.ValueOf(response.VendorCredit).Interface().(T), true
+		}
+	}
+
+	return zero, false
+}
+
+func BatchQueryExtractor[T any](response *BatchItemResponse) []T {
+	if reflect.ValueOf(response.QueryResponse).IsZero() {
+		return nil
+	}
+
+	var zero T
+	typeName := reflect.TypeOf(zero).Name()
+
+	switch typeName {
+	case "Account":
+		if len(response.QueryResponse.Account) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Account{}) {
+			return reflect.ValueOf(response.QueryResponse.Account).Interface().([]T)
+		}
+	case "Bill":
+		if len(response.QueryResponse.Bill) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Bill{}) {
+			return reflect.ValueOf(response.QueryResponse.Bill).Interface().([]T)
+		}
+	case "BillPayment":
+		if len(response.QueryResponse.BillPayment) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(BillPayment{}) {
+			return reflect.ValueOf(response.QueryResponse.BillPayment).Interface().([]T)
+		}
+	case "Class":
+		if len(response.QueryResponse.Class) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Class{}) {
+			return reflect.ValueOf(response.QueryResponse.Class).Interface().([]T)
+		}
+	case "CreditMemo":
+		if len(response.QueryResponse.CreditMemo) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(CreditMemo{}) {
+			return reflect.ValueOf(response.QueryResponse.CreditMemo).Interface().([]T)
+		}
+	case "Customer":
+		if len(response.QueryResponse.Customer) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Customer{}) {
+			return reflect.ValueOf(response.QueryResponse.Customer).Interface().([]T)
+		}
+	case "CustomerType":
+		if len(response.QueryResponse.CustomerType) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(CustomerType{}) {
+			return reflect.ValueOf(response.QueryResponse.CustomerType).Interface().([]T)
+		}
+	case "Deposit":
+		if len(response.QueryResponse.Deposit) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Deposit{}) {
+			return reflect.ValueOf(response.QueryResponse.Deposit).Interface().([]T)
+		}
+	case "Employee":
+		if len(response.QueryResponse.Employee) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Employee{}) {
+			return reflect.ValueOf(response.QueryResponse.Employee).Interface().([]T)
+		}
+	case "Estimate":
+		if len(response.QueryResponse.Estimate) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Estimate{}) {
+			return reflect.ValueOf(response.QueryResponse.Estimate).Interface().([]T)
+		}
+	case "Invoice":
+		if len(response.QueryResponse.Invoice) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Invoice{}) {
+			return reflect.ValueOf(response.QueryResponse.Invoice).Interface().([]T)
+		}
+	case "Item":
+		if len(response.QueryResponse.Item) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Item{}) {
+			return reflect.ValueOf(response.QueryResponse.Item).Interface().([]T)
+		}
+	case "Payment":
+		if len(response.QueryResponse.Payment) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Payment{}) {
+			return reflect.ValueOf(response.QueryResponse.Payment).Interface().([]T)
+		}
+	case "PaymentMethod":
+		if len(response.QueryResponse.PaymentMethod) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(PaymentMethod{}) {
+			return reflect.ValueOf(response.QueryResponse.PaymentMethod).Interface().([]T)
+		}
+	case "Purchase":
+		if len(response.QueryResponse.Purchase) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Purchase{}) {
+			return reflect.ValueOf(response.QueryResponse.Purchase).Interface().([]T)
+		}
+	case "ReimburseCharge":
+		if len(response.QueryResponse.ReimburseCharge) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(ReimburseCharge{}) {
+			return reflect.ValueOf(response.QueryResponse.ReimburseCharge).Interface().([]T)
+		}
+	case "TaxCode":
+		if len(response.QueryResponse.TaxCode) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(TaxCode{}) {
+			return reflect.ValueOf(response.QueryResponse.TaxCode).Interface().([]T)
+		}
+	case "TaxRate":
+		if len(response.QueryResponse.TaxRate) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(TaxRate{}) {
+			return reflect.ValueOf(response.QueryResponse.TaxRate).Interface().([]T)
+		}
+	case "Term":
+		if len(response.QueryResponse.Term) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Term{}) {
+			return reflect.ValueOf(response.QueryResponse.Term).Interface().([]T)
+		}
+	case "TimeActivity":
+		if len(response.QueryResponse.TimeActivity) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(TimeActivity{}) {
+			return reflect.ValueOf(response.QueryResponse.TimeActivity).Interface().([]T)
+		}
+	case "Vendor":
+		if len(response.QueryResponse.Vendor) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Vendor{}) {
+			return reflect.ValueOf(response.QueryResponse.Vendor).Interface().([]T)
+		}
+	case "VendorCredit":
+		if len(response.QueryResponse.VendorCredit) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(VendorCredit{}) {
+			return reflect.ValueOf(response.QueryResponse.VendorCredit).Interface().([]T)
+		}
+	}
+
+	return nil
 }
--- cdc.go
+++ cdc.go
@@ -2,6 +2,7 @@ package quickbooks
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"time"
 )
@@ -48,4 +49,89 @@ func (c *Client) ChangeDataCapture(params RequestParameters, entities []string, 
 		return ChangeDataCapture{}, fmt.Errorf("failed to make change data capture request: %w", err)
 	}
 	return res, nil
+}
+
+func CDCQueryExtractor[T any](response *ChangeDataCapture) []T {
+	var zero T
+	typeName := reflect.TypeOf(zero).Name()
+
+	for _, resp := range response.CDCResponse {
+		for _, query := range resp.QueryResponse {
+			switch typeName {
+			case "Account":
+				if len(query.Account) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Account{}) {
+					return reflect.ValueOf(query.Account).Interface().([]T)
+				}
+			case "Bill":
+				if len(query.Bill) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Bill{}) {
+					return reflect.ValueOf(query.Bill).Interface().([]T)
+				}
+			case "BillPayment":
+				if len(query.BillPayment) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(BillPayment{}) {
+					return reflect.ValueOf(query.BillPayment).Interface().([]T)
+				}
+			case "Class":
+				if len(query.Class) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Class{}) {
+					return reflect.ValueOf(query.Class).Interface().([]T)
+				}
+			case "Customer":
+				if len(query.Customer) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Customer{}) {
+					return reflect.ValueOf(query.Customer).Interface().([]T)
+				}
+			case "CustomerType":
+				if len(query.CustomerType) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(CustomerType{}) {
+					return reflect.ValueOf(query.CustomerType).Interface().([]T)
+				}
+			case "Deposit":
+				if len(query.Deposit) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Deposit{}) {
+					return reflect.ValueOf(query.Deposit).Interface().([]T)
+				}
+			case "Employee":
+				if len(query.Employee) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Employee{}) {
+					return reflect.ValueOf(query.Employee).Interface().([]T)
+				}
+			case "Estimate":
+				if len(query.Estimate) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Estimate{}) {
+					return reflect.ValueOf(query.Estimate).Interface().([]T)
+				}
+			case "Invoice":
+				if len(query.Invoice) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Invoice{}) {
+					return reflect.ValueOf(query.Invoice).Interface().([]T)
+				}
+			case "Item":
+				if len(query.Item) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Item{}) {
+					return reflect.ValueOf(query.Item).Interface().([]T)
+				}
+			case "Payment":
+				if len(query.Payment) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Payment{}) {
+					return reflect.ValueOf(query.Payment).Interface().([]T)
+				}
+			case "PaymentMethod":
+				if len(query.PaymentMethod) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(PaymentMethod{}) {
+					return reflect.ValueOf(query.PaymentMethod).Interface().([]T)
+				}
+			case "Purchase":
+				if len(query.Purchase) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Purchase{}) {
+					return reflect.ValueOf(query.Purchase).Interface().([]T)
+				}
+			case "ReimburseCharge":
+				if len(query.ReimburseCharge) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(ReimburseCharge{}) {
+					return reflect.ValueOf(query.ReimburseCharge).Interface().([]T)
+				}
+			case "Term":
+				if len(query.Term) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Term{}) {
+					return reflect.ValueOf(query.Term).Interface().([]T)
+				}
+			case "Vendor":
+				if len(query.Vendor) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(Vendor{}) {
+					return reflect.ValueOf(query.Vendor).Interface().([]T)
+				}
+			case "VendorCredit":
+				if len(query.VendorCredit) > 0 && reflect.TypeOf(zero) == reflect.TypeOf(VendorCredit{}) {
+					return reflect.ValueOf(query.VendorCredit).Interface().([]T)
+				}
+			}
+		}
+	}
+	return nil
 }
